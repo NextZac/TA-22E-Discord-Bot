3290b042d8aea6567fbf2d6bedb539402e322d11
NextZac <48414018+NextZac@users.noreply.github.com>
2022-09-07 18:28:12 +0300

    Update reminder.js

--- events/reminder.js
+++ events/reminder.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { EmbedBuilder } = require('discord.js');
 const schedule = require('node-schedule')
 const client = require('..');
 
@@ -6,12 +7,18 @@ module.exports.lessonCheck = schedule.scheduleJob("00 07 * * *", async function(
     const today = new Date();
     const data = await (await axios.get(`https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${today.toISOString()}&studentGroups=6932&thru=${today.toISOString()}`)).data;
     for(i in data) {
-        const [min, hour] = [Number(data[i]['timeStart'].split(":")[1]), Number(data[i]['timeStart'].split(":")[0])];
+        const [min, hour] = [Number(data[i]['timeStart'].toString().split(":")[1]), Number(data[i]['timeStart'].toString().split(":")[0])];
         schedule.scheduleJob(`${min - 5} ${hour} * * *`, async function() {
-            client.channels.cache.get('1016342648279289907').send(`Tund: ${data[i]['nameEt']} hakkab 5 minuti pärast!\nKlassis: ${data[i]['rooms'][0]['buildingCode']}-${data[i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`);
+            const embed = new EmbedBuilder()
+                .setTitle(`Tund ${data[i]['nameEt'].toString()}`)
+                .setDescription(`Tund algab 5 minuti pärast!\n${data[i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`)
+            client.channels.cache.get('1016342648279289907').send({embed: [embed]});
       });
       schedule.scheduleJob(`${min} ${hour} * * *`, async function() {
-            client.channels.cache.get('1016342648279289907').send(`Tund: ${timetableEvent.nameEt} hakkas!\nKlassis: ${data[i]['rooms'][0]['buildingCode']}-${data[i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`);
+        const embed = new EmbedBuilder()
+            .setTitle(`Tund ${data[i]['nameEt'].toString()}`)
+            .setDescription(`Tund algas!\n${data[i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`)
+        client.channels.cache.get('1016342648279289907').send({embed: [embed]});
       });
 
     }
