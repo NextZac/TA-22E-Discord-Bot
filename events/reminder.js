a3e8b4c04b5b5d3cd6d86580de297bcebef46619
NextZac <pzsebastian@gmail.com>
2022-09-30 09:21:12 +0300

    tried to fix some parsing errors (fuck my life)

--- events/reminder.js
+++ events/reminder.js
@@ -4,19 +4,19 @@ const schedule = require('node-schedule')
 const client = require('..');
 const chalk = require('chalk')
 
-module.exports.lessonCheck = schedule.scheduleJob("00 07 * * *", async function() {
+schedule.scheduleJob("00 07 * * *", async function() {
     const today = new Date();
-    const data = await (await axios.get(`https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${today.toISOString()}&studentGroups=6932&thru=${today.toISOString()}`)).data;
-    for(i in data['data']['timetableEvents']) {
-        const [date, min, hour] = [new Date(data[i]['date']), Number(data[i]['timeStart'].split(":")[1]), Number(data[i]['timeStart'].split(":")[0])];
-        const channel = client.channels.cache.get('1016342648279289907');
-        data.setMinutes(min - 5);
+    const data = await (await axios.get(`https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${today.toISOString()}&studentGroups=6934&thru=${today.toISOString()}`)).data;
+    for(i in data['timetableEvents']) {
+        const [date, min, hour] = [new Date(data['timetableEvents'][i]['date']), Number(data['timetableEvents'][i]['timeStart'].split(":")[1]), Number(data['timetableEvents'][i]['timeStart'].split(":")[0])];
+        const channel = client.channels.cache.get('1015307760470073374');
+        date.setMinutes(min - 5);
         date.setHours(hour);
         schedule.scheduleJob(date, async function() {
             date.setMinutes(min)
             const embed = new EmbedBuilder()
                 .setTitle(`Tund ${data[i]['nameEt'].toString()}`)
-                .setDescription(`Tund algab <t:${Math.floor(date.floor(date.getTime() / 1000))}>!\nKlass:${data[i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`)
+                .setDescription(`Tund algab <t:${Math.floor(date.floor(date.getTime() / 1000))}>!\nKlass:${data['timetableEvents'][i]['rooms'][0]['roomCode']}\n<@1016731276989972511>`)
             await channel.send({embeds: [embed]});
             console.log(chalk.yellow(`[Reminder] `) + `${channel.name}`)
       });
